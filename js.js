fb7e5bce96614faad186cbb2ee643ede8601badb
xaker1020 <qodirjonovhabibulloh8@gmail.com>
2025-04-16 14:38:03 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+let ism=prompt("string type da malumot kiriting")
+let num=prompt("number type da malumot kiriting")
+let bool=prompt("boolean type da malumot kiriting")
+confirm("Hammasi to'g'rimi ")
+alert(` Siz kiritgan ${ism} sozi ${typeof ism}  type da \nSiz kiritgan ${num} raqami ${typeof num}
+    type d\nSiz kiritgan ${bool}sozi booleant type  `)
+
